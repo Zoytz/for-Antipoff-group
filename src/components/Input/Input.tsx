161237204292ba2af7ff6877ecd27c445f61f4ea
Alexey Spiridonov <ZayacZloy@yandex.ru>
2022-12-22 13:13:27 +0300

    fix: added email input pattern

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -10,12 +10,19 @@ type PropsType = {
   onChange: (e: ChangeEvent<HTMLInputElement>) => void
 }
 
+const emailPattern = '\\w+([\\.-]?\\w+)*@\\w+([\\.-]?\\w+)*\\.\\w{2,4}';
+
 const Input: FC<PropsType> = ({ type, error, name, label, required, onChange, value }) => {
   return (
+
     <label htmlFor={name} className="input__label">
       {label}
-      <input value={value || ''} onChange={onChange} required={required} type={type} className={`input ${error ? "input_type_error" : ""}`} name={name} />
-      <span className={`input__error ${error ? "input__error_type_visible" : ""}`}>{error}</span>
+      {type === "email" ?
+        <input pattern={emailPattern} value={value || ''} onChange={onChange} required={required} type={type} className={`input ${error ? "input_type_error" : ""}`} name={name} />
+        :
+        <input value={value || ''} onChange={onChange} required={required} type={type} className={`input ${error ? "input_type_error" : ""}`} name={name} />
+      }
+      <span className={`input__error ${error ? "input__error_type_visible" : ""}`}>{error === "Введите данные в указанном формате." ? "Введите данные в формате example@example.ru." : error}</span>
     </label>
   )
 }
